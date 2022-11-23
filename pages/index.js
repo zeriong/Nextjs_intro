5404ee1715617e15906d43dcf8514f9d2d7558c5
zeriong <jaeryong95@gmail.com>
2022-11-23 22:39:13 +0900

    TMDB에서 API를 받아오고 CSS를 적용시켰다.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
-import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
+import {useEffect, useState} from "react";
+
+const API_KEY = "bdbcd6692864a7e7aa26a053758354ac"
 
 export default function Home() {
+    const [movies, setMovies] = useState();
+    useEffect(() => {
+        (async () => {
+            const { results } = await (
+                await fetch(
+                    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+                )
+            ).json();
+            setMovies(results);
+        })();
+    }, []);
     return (
         <div>
-            <h1>Hello</h1>
+            <Seo title="Home" />
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map((movie) => (
+                <div key={movie.id}>
+                    <h4>{movie.original_title}</h4>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
